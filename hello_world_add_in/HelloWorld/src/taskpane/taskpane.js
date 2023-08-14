766a4339698b38d44320b4d1d03a5aeaebc43960
apros7 <lucas.vilsen@gmail.com>
2023-08-14 13:47:29 +0200

    automatically gets text now

--- hello_world_add_in/HelloWorld/src/taskpane/taskpane.js
+++ hello_world_add_in/HelloWorld/src/taskpane/taskpane.js
@@ -15,16 +15,40 @@ Office.onReady((info) => {
 
 export async function run() {
   return Word.run(async (context) => {
+    const info_text_html = document.getElementById("info-text");
+    var documentBody = context.document.body;
+    context.load(documentBody);
+    // info_text_html.textContent = "Hej med dig"
     /**
      * Insert your Word code here
      */
 
     // insert a paragraph at the end of the document.
-    const paragraph = context.document.body.insertParagraph("Hello World", Word.InsertLocation.end);
+    // const paragraph = context.document.body.insertParagraph("Hello World", Word.InsertLocation.end);
 
-    // change the paragraph color to blue.
-    paragraph.font.color = "blue";
+    // // change the paragraph color to blue.
+    // paragraph.font.color = "blue";
 
-    await context.sync();
+    const paragraphs = documentBody.paragraphs;
+    paragraphs.load("text");
+
+    await context.sync()
+
+    const textContent = paragraphs.items.map(paragraph => paragraph.text).join('<br>');
+
+    info_text_html.innerHTML = textContent
+
+    update_info_text(context)
   });
+}
+
+async function update_info_text(context) {
+  setInterval(() => {
+    const info_text_html = document.getElementById("info-text");
+    var documentBody = context.document.body;
+    context.load(documentBody);
+    context.sync().then(() => {
+      info_text_html.innerHTML = documentBody.text;
+    })
+  }, 10000)
 }
