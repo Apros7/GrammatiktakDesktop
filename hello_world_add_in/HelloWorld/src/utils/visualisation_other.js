0daaf5a04dd9e6226542552bc995bd0d6e2e1ba1
apros7 <lucas.vilsen@gmail.com>
2023-08-25 10:34:06 +0200

    cleaner code

--- hello_world_add_in/HelloWorld/src/utils/visualisation_other.js
+++ hello_world_add_in/HelloWorld/src/utils/visualisation_other.js
@@ -11,20 +11,19 @@ export function check_clear_message(sentence_information) {
 }
 
 export function activate_spinner() {
-  document.getElementById("extra2").textContent = "Hey1"
   const error_visualize_section = document.getElementById("errors-visualized");
-  document.getElementById("extra2").textContent = "Hey2"
   error_visualize_section.innerHTML = "";
-  document.getElementById("extra2").textContent = "Hey3"
+
   const background = document.createElement("div");
-  document.getElementById("extra2").textContent = "Hey4"
   background.classList.add("spinner-background")
-  document.getElementById("extra2").textContent = "Hey5"
+
   let text = document.createElement("div");
   text.classList.add("spinner-text")
   text.innerText = "Vi retter din tekst..."
+
   const spinner = document.createElement("div");
   spinner.classList.add("spinner")
+  
   background.appendChild(spinner)
   background.appendChild(text)
   error_visualize_section.appendChild(background)
