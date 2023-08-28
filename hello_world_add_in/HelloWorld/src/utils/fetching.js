0101d5deb9c1ba0893a6fced6f5bf8a0553b242e
apros7 <lucas.vilsen@gmail.com>
2023-08-28 11:24:23 +0200

    found bug lets go

--- hello_world_add_in/HelloWorld/src/utils/fetching.js
+++ hello_world_add_in/HelloWorld/src/utils/fetching.js
@@ -1,7 +1,7 @@
 
 export function fetchFeedback(service_url, text, feedback = null) {
     if (feedback === null) {feedback = document.querySelector(".feedback-text").innerText;}
-    let object = {"sentence": get_text(), "feedback": feedback};
+    let object = {"sentence": text, "feedback": feedback};
     fetch(service_url, {
       method: 'POST',
       headers: {
@@ -12,13 +12,14 @@ export function fetchFeedback(service_url, text, feedback = null) {
 }
 
 export async function fetchData(service_url, chunk, sentence_information) {
+  // document.getElementById("extra2").textContent = JSON.stringify([chunk, differences, fixed_chunk === "Hej jeg hedder Lucas"], null, 2)
   let object = {"sentence": chunk, "feedback": null};
   const response = await fetch(service_url, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(object)
+    }, 
+    body: JSON.stringify(object) 
   });
   if (!response.ok) {
     return "error"
@@ -27,7 +28,7 @@ export async function fetchData(service_url, chunk, sentence_information) {
   const errors = JSON.parse(data.replace(/\\u([a-f0-9]{4})/gi, (match, group) => String.fromCharCode(parseInt(group, 16))));
   sentence_information.waiting_for_backend[chunk] = false
   sentence_information.errors_matching_text[chunk] = errors
-  document.getElementById("extra").textContent = JSON.stringify(errors, null, 2)
+  // document.getElementById("extra2").textContent = JSON.stringify([chunk, errors], null, 2)
   return errors
 }
 
